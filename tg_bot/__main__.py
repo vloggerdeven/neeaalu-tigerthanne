556c4739e61ac6e90dd747096b92303576f390c8
dk [OFLINE] <98028377+vloggerdeven@users.noreply.github.com>
2022-04-23 00:29:29 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -136,8 +136,6 @@ def start(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
 
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup
-
     else:
         update.effective_message.reply_text("ചത്തിട്ടില്ലാ...")
 
